ba27ef41fb810be85712e87ded874c92d21408dd
Alya <a.slipenka@gmail.com>
2025-07-28 00:34:15 +0300

    modal is connected

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,16 +1,31 @@
-
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-      document.body.classList.toggle("no-scroll");
-    }
+  const openButtons = document.querySelectorAll("[data-modal-open-10], [data-modal-open-65], [data-modal-open-80], [data-modal-open-80-pads]");
+  const closeButtons = document.querySelectorAll("[data-modal-close]");
+
+  const modalMap = {
+    "data-modal-open-10": "10",
+    "data-modal-open-65": "65",
+    "data-modal-open-80": "80",
+    "data-modal-open-80-pads": "80-pads"
+  };
+
+  openButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      for (const key in modalMap) {
+        if (button.hasAttribute(key)) {
+          const modal = document.querySelector(`[data-modal='${modalMap[key]}']`);
+          modal.classList.remove("is-hidden");
+          document.body.classList.add("no-scroll");
+        }
+      }
+    });
+  });
+
+  closeButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      const modal = button.closest(".modal-overlay");
+      modal.classList.add("is-hidden");
+      document.body.classList.remove("no-scroll");
+    });
+  });
 })();
